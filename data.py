b8ae2ca4e62e95c8c0518bcd8deea04b31ccf936
sayhi <1045541554@qq.com>
2020-05-30 13:44:28 +0800

    .

---
+++ data.py
@@ -0,0 +1,53 @@
+import numpy as np
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, roc_auc_score, accuracy_score
+
+def get_data():
+    train_root = "dataset/train/"
+    train_user_path = os.path.join(train_root, "user.csv")
+    train_ad_path = os.path.join(train_root, "ad.csv")
+    train_click_path = os.path.join(train_root, "click_log.csv")
+    test_root = "dataset/test/"
+    test_ad_path = os.path.join(test_root, "ad.csv")
+    test_click_path = os.path.join(test_root, "click_log.csv")
+    
+    train_ad = pd.read_csv(train_ad_path, na_values="\\N")
+    train_click = pd.read_csv(train_click_path, na_values="\\N")
+    train_user = pd.read_csv(train_user_path, na_values="\\N")
+    test_ad = pd.read_csv(test_ad_path, na_values="\\N")
+    test_click = pd.read_csv(test_click_path, na_values="\\N")
+    
+    return train_ad, train_click, train_user, test_ad, test_click
+
+
+def get_part_click(total_click, list_user_id, on="user_id"):
+    part_record = pd.merge(total_click, list_user_id, on=on)
+    return part_record
+
+
+def get_ad_inform(creative_id, data_ad):
+    ad_inform = data_ad[data_ad["creative_id"] == creative_id]
+#     print(ad_inform.astype(int))
+    return ad_inform.astype(int)
+
+
+def split_feature_target(raw_features):
+    train_features = raw_features.iloc[:, [0, 2, 3, 6, 7, 8, 9, 10]]
+    train_age = raw_features.iloc[:, 4]
+    train_gender = raw_features.iloc[:, 5]
+    
+    return train_features, train_age, train_gender
+
+def measure_unique_user(record_pred, data_record, data_user, column_name="gender"):
+    df_pred = pd.DataFrame(data_record.user_id)
+    df_pred[column_name] = np.array(record_pred)
+    
+    uni_user_pred = df_pred.groupby("user_id").agg({column_name: lambda x: x.value_counts().index[0]})
+    pred = uni_user_pred.iloc[:, 0].values + 1
+    target = data_user.sort_values("user_id")[column_name].values
+    acc_score = accuracy_score(pred, target)
+    
+    return uni_user_pred, acc_score
---
+++ model.py
@@ -0,0 +1,112 @@
+import data
+import lightgbm as lgb
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class lgb_model():
+    def __init__(self, model_kind="gender"):
+        '''
+        @train_dataset: lgb.Dataset(X, y)
+        @valid_dataset: lgb.Dataset(X, y)
+        '''
+#         self.train_dataset = train_dataset
+#         self.valid_dataset = valid_dataset
+        
+        self.params = {
+            'task': 'train',
+            'boosting_type': 'gbdt',  # 设置提升类型
+            'num_leaves': 47,  # 叶子节点数
+            'learning_rate': 0.1,  # 学习速率
+            'feature_fraction': 0.9,  # 建树的特征选择比例
+            'bagging_fraction': 0.8,  # 建树的样本采样比例
+            'lambda_l2': 0.01,
+            'bagging_freq': 5,  # k 意味着每 k 次迭代执行bagging
+            'verbose': 1  # <0 显示致命的, =0 显示错误 (警告), >0 显示信息
+        }
+        self.categorical_feature = [1, 3, 4, 5, 6, 7]
+        self.num_interations = 100
+        
+        self.model_kind = model_kind
+        if self.model_kind == "gender":
+            self.params["metric"] = {"binary_logloss", "auc"}
+            self.params["objective"] = "binary"
+            self.model_save_path = "checkpoints/gender_model.pkl"
+        elif self.model_kind == "age":
+            self.params["metric"] = {"softmax"}
+            self.params["objective"] = "multiclass"
+            self.params['num_class'] = 10
+            self.model_save_path = "checkpoints/age_model.pkl"
+
+    def train(self, train_dataset, valid_dataset):
+        self.gbm = lgb.train(self.params,
+                        train_dataset,
+                        num_boost_round=self.num_interations,
+                        valid_sets=valid_dataset,
+                        early_stopping_rounds=10,
+                        categorical_feature=self.categorical_feature)        
+    
+    def get_model(self):
+        return self.gbm
+    
+    def save_model(self):
+        self.gbm.save_model(self.model_save_path)
+        
+    def load_model(self):
+        self.gbm = lgb.Booster(model_file=self.model_save_path)
+        
+    def predict(self, input_features):
+        pred = self.gbm.predict(input_features, num_iteration=self.gbm.best_iteration)
+        return pred
+    
+    def transform_pred(self, pred):
+        if self.model_kind == "gender":
+            record_pred_label = pred.copy()
+            record_pred_label[pred >= 0.5] = 1
+            record_pred_label[pred < 0.5] = 0
+            record_acc = accuracy_score(record_pred_label.astype(int), valid_gender)
+        elif self.model_kind == "age":
+            record_pred_label = [list(x).index(max(x)) for x in pred]
+            record_acc = accuracy_score(np.array(age_pred_label), valid_age)
+            
+        print("accuracy for {} is: {:.5f}".format(self.model_kind, record_acc))
+        
+        return record_pred_label
+    
+    def measure(self, record_pred_label, data_record, data_user):
+        uni_pred, uni_acc = data.measure_unique_user(record_pred_label, data_record, data_user, self.model_kind)
+        return uni_pred, uni_acc
+
+    
+    
+class lstm_model(nn.Module):
+    def __init__(self, input_dim embedding_dim, hidden_dim, output_dim):
+        super(lstm_model, self).__init__()
+        self.input_dim = input_dim
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.output_dim = output_dim
+
+        self.word_embeddings = nn.Embedding(input_dim, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim)
+        self.fc = nn.Linear(hidden_dim, output_dim, bias=True)
+        self.dropout = nn.Dropout(p=0.6)
+        
+
+    def forward(self, x):
+#         embeds = self.word_embeddings(x)
+#         lstm_out, _ = self.lstm(embeds)
+        
+        h0 = torch.randn(self.num_layers, self.batch_size, self.hidden_dim)
+        c0 = torch.randn(self.num_layers, self.batch_size, self.hidden_dim)
+        h0 = h0.to(self.device)
+        c0 = c0.to(self.device)
+
+        x_embed = self.word_embeddings(x)
+        output, hidden_state = self.lstm(x_embed, (h0, c0))
+        output = output.contiguous().view(output.shape[0] * output.shape[1], -1)
+        output = self.fc(self.dropout(output))
+        
+        return output
